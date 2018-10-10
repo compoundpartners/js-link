6afb698c5cf3ece38231d82d8ab81b175f70a225
Singh <singh@compoundpartners.co.uk>
2018-10-10 11:03:52 +0100

    GAT01-99  >  js-list (Link List & Link Plugins)  >  Load & display text in admin

--- js_link/models.py
+++ js_link/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from cms.models import CMSPlugin
 from filer.fields.image import FilerImageField
 
+from django.utils.html import strip_tags
+from django.utils.text import Truncator
+
 
 class JSLinkList(CMSPlugin):
 
     title = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return Truncator(strip_tags(self.title)).words(3, truncate="...")
 
 
 class JSLink(CMSPlugin):
@@ -14,5 +20,5 @@ class JSLink(CMSPlugin):
     text = models.CharField(max_length=255, blank=True)
     url = models.CharField(max_length=255, blank=True)
 
-    def __unicode__(self):
-        return self.text()
+    def __str__(self):
+        return Truncator(strip_tags(self.text)).words(3, truncate="...")
